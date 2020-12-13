ad71074d63894b44fffef95aecbf58b87491839a
Ryan Prince <judascleric@gmail.com>
2020-12-12 16:43:39 -0800

    small fixes

--- scenes/main-scene.ts
+++ scenes/main-scene.ts
@@ -1,8 +1,6 @@
 export class MainScene extends Phaser.Scene {
     private phaserSprite: Phaser.GameObjects.Sprite;
 
-    TEST = ["a", "b", "c"];
-
     constructor() {
         super({
             key: "MainScene",
--- webpack.config.js
+++ webpack.config.js
@@ -3,32 +3,32 @@ var pathToPhaser = path.join(__dirname, "/node_modules/phaser/");
 var phaser = path.join(pathToPhaser, "dist/phaser.js");
 
 module.exports = {
-  entry: "./src/picross.ts",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js"
-  },
-  module: {
-    rules: [
-      { test: /\.ts$/, loader: "ts-loader", exclude: "/node_modules/" },
-      { test: /phaser\.js$/, loader: "expose-loader", options: {exposes: "Phaser"} }
-    ]
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, "./"),
-    publicPath: "/dist/",
-    host: "127.0.0.1",
-    port: 8080,
-    open: true
-  },
-  resolve: {
-    extensions: [".ts", ".js"],
-    alias: {
-      phaser: phaser
-    }
-  },
-  performance: {
-    maxEntrypointSize: 2048000,
-    maxAssetSize: 2048000
-  }
+    entry: "./src/nononoyesgram.ts",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+    },
+    module: {
+        rules: [
+            { test: /\.ts$/, loader: "ts-loader", exclude: "/node_modules/" },
+            { test: /phaser\.js$/, loader: "expose-loader", options: { exposes: ["Phaser"] } },
+        ],
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, "./"),
+        publicPath: "/dist/",
+        host: "127.0.0.1",
+        port: 8080,
+        open: true,
+    },
+    resolve: {
+        extensions: [".ts", ".js"],
+        alias: {
+            phaser: phaser,
+        },
+    },
+    performance: {
+        maxEntrypointSize: 2048000,
+        maxAssetSize: 2048000,
+    },
 };
